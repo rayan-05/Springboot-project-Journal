5c0909990458a2435c02c361efefd3bcb095e9b7
Rayan Mitra <rayan.mitra2021@vitstudent.ac.in>
2025-04-01 00:24:44 +0530

    logging and creating log files

--- src/main/java/com/rayanmitra/journalApp/service/UserService.java
+++ src/main/java/com/rayanmitra/journalApp/service/UserService.java
@@ -2,7 +2,10 @@ package com.rayanmitra.journalApp.service;
 
 import com.rayanmitra.journalApp.entity.User;
 import com.rayanmitra.journalApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,17 +16,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+    public boolean saveNewUser(User user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+            return true;
+        }catch (Exception e){
+            log.error("Error occured for {}",user.getUserName(), e);
+            return false;
+        }
     }
 
     public void saveAdmin(User user) {

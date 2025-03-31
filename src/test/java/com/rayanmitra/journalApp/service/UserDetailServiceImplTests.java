d041307338f02434af84128908d59bcfd56b6ad9
Rayan Mitra <rayan.mitra2021@vitstudent.ac.in>
2025-03-31 15:27:33 +0530

    using mock for testing

---
+++ src/test/java/com/rayanmitra/journalApp/service/UserDetailServiceImplTests.java
@@ -0,0 +1,48 @@
+package com.rayanmitra.journalApp.service;
+
+import com.rayanmitra.journalApp.entity.User;
+import com.rayanmitra.journalApp.repository.UserRepository;
+import lombok.experimental.Accessors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+
+public class UserDetailServiceImplTests {
+
+    @InjectMocks
+    private UserDetailServiceImpl userDetailService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeMethod
+    public void setUp() {
+        // Initialize mocks before each test
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("ram").password("jdsbkfjbskdjfb").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+
+    }
+
+}
